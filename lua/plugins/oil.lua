5a08be249a838034a00defa04b5ba331723149da
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-02-17 21:19:56 +0200

    style:changing oil and toggle-term size

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,21 +8,20 @@
 -- }
 --
 --
- return {
-  "stevearc/oil.nvim",
-  config = function()
-    local oil = require("oil")
-    oil.setup({
-      keymaps = {
-        ["H"] = "actions.parent", -- Navigate to parent directory
-        ["L"] = "actions.select", -- Open directory or file
-      },
-      float = {
-        max_width = 80,  -- Maximum width of the floating window
-        max_height = 20, -- Maximum height of the floating window
-      },
-    })
-    vim.keymap.set("n", "t", oil.toggle_float, {})
-  end,
+return {
+	"stevearc/oil.nvim",
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			keymaps = {
+				["H"] = "actions.parent", -- Navigate to parent directory
+				["L"] = "actions.select", -- Open directory or file
+			},
+			float = {
+				max_width = 50, -- Maximum width of the floating window
+				max_height = 15, -- Maximum height of the floating window
+			},
+		})
+		vim.keymap.set("n", "t", oil.toggle_float, {})
+	end,
 }
-
