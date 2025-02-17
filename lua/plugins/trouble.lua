b9c6acb9a947a02219be6df78e8be30141954af2
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-02-17 04:41:59 +0200

    feat:adding trouble.nvim

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,56 @@
+-- Lua
+return {
+	"folke/trouble.nvim",
+	requires = "kyazdani42/nvim-web-devicons",
+	config = function()
+		require("trouble").setup()
+
+		-- Toggle Trouble (default view, shows workspace diagnostics)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>xx",
+			"<cmd>TroubleToggle<cr>",
+			{ silent = true, noremap = true, desc = "Toggle Trouble" }
+		)
+
+		-- Show workspace-wide diagnostics (all files)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>xw",
+			"<cmd>TroubleToggle workspace_diagnostics<cr>",
+			{ silent = true, noremap = true, desc = "Show workspace diagnostics" }
+		)
+
+		-- Show document diagnostics (current file only)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>xd",
+			"<cmd>TroubleToggle document_diagnostics<cr>",
+			{ silent = true, noremap = true, desc = "Show document diagnostics" }
+		)
+
+		-- Show location list (used by :lopen / :lclose, often used for errors in a single file)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>xl",
+			"<cmd>TroubleToggle loclist<cr>",
+			{ silent = true, noremap = true, desc = "Show location list" }
+		)
+
+		-- Show quickfix list (used by :copen / :cclose, common for grep results and build errors)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>xq",
+			"<cmd>TroubleToggle quickfix<cr>",
+			{ silent = true, noremap = true, desc = "Show quickfix list" }
+		)
+
+		-- Show LSP references (when cursor is on a symbol, shows all references)
+		vim.api.nvim_set_keymap(
+			"n",
+			"gR",
+			"<cmd>TroubleToggle lsp_references<cr>",
+			{ silent = true, noremap = true, desc = "Show LSP references" }
+		)
+	end,
+}
