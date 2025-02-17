1b3c7887112d08311822dcdcbfa5c7a941152ce8
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-02-17 04:37:57 +0200

    feat:json support with schema store and lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,26 +17,50 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+
+      -- TypeScript
       lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
 
+      -- Ruby
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+      })
+
+      -- HTML
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      -- JSON Language Server (jsonls) with SchemaStore
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(), -- Auto-fetch schemas
+            validate = { enable = true },
+          },
+        },
+      })
+
+      -- Keybindings
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
+  },
+  -- SchemaStore for JSON schema support
+  {
+    "b0o/schemastore.nvim",
+    lazy = true,
   },
 }
