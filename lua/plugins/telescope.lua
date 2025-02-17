1ec61a3e3c1a7d66a152d48b08ffe2f596110c6c
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-02-17 04:21:43 +0200

    feat:a better new telescope ui,ignore unneeded file
    Now you can see preview of files.
    simple dropdown ui.
    ignore node_module and .git folder

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,74 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			-- local trouble = require("trouble.providers.telescope") -- seems like a wrong provider
+			-- local themes = require("telescope.themes")
 
-      telescope.setup({
-        defaults = {
-          mappings = {
-            i = { ["<C-u>"] = false, ["<C-d>"] = false },
-          },
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+			-- Custom dropdown theme with large dimensions
+			local dropdown_theme = require("telescope.themes").get_dropdown({
+				layout_config = {
+					width = 0.80, -- Single width definition
+					height = 0.70, -- Single width definition
+					preview_width = 0.6, -- Preview takes 60% of the width
+					-- Corrected typo
+					prompt_position = "top",
+				},
 
-      -- Keymaps
-      vim.keymap.set("n", "<leader> ", builtin.find_files, {}) -- Find files
-      vim.keymap.set("n", "<leader>fw", builtin.live_grep, {}) -- Live grep
-      vim.keymap.set("n", "<leader>o", builtin.oldfiles, {}) -- Recent files
-      vim.keymap.set("n", "<leader>b", builtin.buffers, {}) -- Search buffers
-      vim.keymap.set("n", "<leader>fk", builtin.keymaps, {}) -- List keymaps
-      vim.keymap.set("n", "<leader>fc", builtin.commands, {}) -- List commands
-      vim.keymap.set("n", "<leader>gf", builtin.git_files, {}) -- Git files (if in repo)
+				layout_strategy = "horizontal",
+				winblend = 2, -- Slight transparency
+			})
+			telescope.setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules", ".git" },
+					mappings = {
+						-- i = { ["<c-t>"] = trouble.open_with_trouble },
+						-- n = { ["<c-t>"] = trouble.open_with_trouble },
+						i = { ["<C-u>"] = false, ["<C-d>"] = false },
+					},
+				},
+				extensions = {
+					["ui-select"] = dropdown_theme,
+				},
+			})
 
-      -- Load extensions
-      telescope.load_extension("ui-select")
-    end,
-  },
+			-- Keymaps with dropdown theme applied
+			vim.keymap.set("n", "<leader> ", function()
+				builtin.find_files(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>fw", function()
+				builtin.live_grep(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>o", function()
+				builtin.oldfiles(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>b", function()
+				builtin.buffers(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>fk", function()
+				builtin.keymaps(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>fc", function()
+				builtin.commands(dropdown_theme)
+			end)
+
+			vim.keymap.set("n", "<leader>gf", function()
+				builtin.git_files(dropdown_theme)
+			end)
+
+			telescope.load_extension("ui-select")
+		end,
+	},
 }
