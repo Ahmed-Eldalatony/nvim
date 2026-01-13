8ce3eb8dd2d2dea24fd468daea7b61e4ae5cbcee
Ahmed Eldalatony <a01141399246@gmail.com>
2026-01-13 23:52:10 +0200

    Adding find and replace for current project

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -16,16 +16,66 @@ return {
       "<leader>sr",
       function()
         local grug = require("grug-far")
-        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        local prefills = {
+          paths = vim.fn.expand("%"),
+        }
+        if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
+          -- Get visual selection
+          local start_pos = vim.fn.getpos("'<")
+          local end_pos = vim.fn.getpos("'>")
+          local lines = vim.api.nvim_buf_get_text(0, start_pos[2]-1, start_pos[3]-1, end_pos[2]-1, end_pos[3], {})
+          prefills.search = table.concat(lines, '\n')
+        end
         grug.open({
           transient = true,
-          prefills = {
-            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
-          },
+          prefills = prefills,
         })
       end,
       mode = { "n", "v" },
       desc = "Search and Replace",
     },
+    {
+      "<leader>gr",
+      function()
+        local grug = require("grug-far")
+        local prefills = {}
+        if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
+          -- Get visual selection
+          local start_pos = vim.fn.getpos("'<")
+          local end_pos = vim.fn.getpos("'>")
+          local lines = vim.api.nvim_buf_get_text(0, start_pos[2]-1, start_pos[3]-1, end_pos[2]-1, end_pos[3], {})
+          prefills.search = table.concat(lines, '\n')
+        end
+        grug.open({
+          transient = true,
+          prefills = prefills,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace (Global)",
+    },
+    -- {
+    --   "<leader>fw",
+    --   function()
+    --     local grug = require("grug-far")
+    --     local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+    --     local prefills = {
+    --       filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+    --     }
+    --     if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
+    --       -- Get visual selection
+    --       local start_pos = vim.fn.getpos("'<")
+    --       local end_pos = vim.fn.getpos("'>")
+    --       local lines = vim.api.nvim_buf_get_text(0, start_pos[2]-1, start_pos[3]-1, end_pos[2]-1, end_pos[3], {})
+    --       prefills.search = table.concat(lines, '\n')
+    --     end
+    --     grug.open({
+    --       transient = true,
+    --       prefills = prefills,
+    --     })
+    --   end,
+    --   mode = { "n", "v" },
+    --   desc = "Search Word",
+    -- },
   },
 }
